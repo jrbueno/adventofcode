9a3b9bf074d16bddb4df6bde60e520b389024d86
Jose Bueno <jrbueno@MacBook-Pro.local>
2023-12-03 22:37:40 -0500

    completed day 1

--- 2023/Day01/Solution.cs
+++ 2023/Day01/Solution.cs
@@ -32,7 +32,7 @@ class Solution : Solver {
                     tmp = c;
                 }
             }
-            last = tmp ?? first;
+            last = tmp;
 
             var combine = $"{first}{last}";
             // Console.WriteLine("First:{0} Last:{1} = {0}{1}", first, last);
@@ -46,35 +46,39 @@ class Solution : Solver {
         foreach (var line in input.Split("\n"))
         {
             Console.WriteLine(line);
-            int offset = 0;
-            string? first = null;
-            string? last;
+            int first;
+            int last;
             string? tmp = null;
+            Dictionary<string, int> charNumbers = new Dictionary<string, int>
+            {
+                { "one", 1},
+                { "two", 2},
+                { "three", 3},
+                { "four", 4},
+                { "five", 5},
+                { "six", 6},
+                { "seven", 7},
+                {"eight", 8},
+                {"nine", 9}
+            };
+            var lineNumbers =
+                charNumbers.Select(kv =>
+                {
+                    var fx = line.IndexOf(kv.Key, StringComparison.OrdinalIgnoreCase);
+                    var lx = line.LastIndexOf(kv.Key, StringComparison.OrdinalIgnoreCase);
+                    return new { Key = kv.Value, FirstIndex = fx, LastIndex = lx };
+                }).Where(kv => kv.FirstIndex >= 0).ToList();
             for (int i = 0; i < line.Length; i++)
             {
-                Console.WriteLine(line[offset..i]);
-                tmp = line[offset..i] switch
-                {
-                    "one" => "1",
-                    "two" => "2",
-                    "three" => "3",
-                    "four" => "4",
-                    "five" => "5",
-                    "six" => "6",
-                    "seven" => "7",
-                    "eight" => "8",
-                    "nine" => "9",
-                    _ => null
-                };
-                if (tmp is null && !line[i].IsDigit()) continue;
-                first ??= tmp;
-                offset = i + 1;
+                if (!line[i].IsDigit()) continue;
+                lineNumbers.Add(new { Key = int.Parse(line[i].ToString()), FirstIndex = i, LastIndex = i });
             }
-            last = tmp ?? first;
+
+            first = lineNumbers.MinBy(kv => kv.FirstIndex).Key;
+            last = lineNumbers.MaxBy(kv => kv.LastIndex).Key;
             var combine = $"{first}{last}";
             Console.WriteLine("First:{0} Last:{1} = {0}{1}", first, last);
-            // total += int.Parse(combine);
-            break;
+            total += int.Parse(combine);
         }
         return total;
     }
