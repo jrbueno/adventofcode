31f3252d94fe0ed694c42742d695b662dbd7db8d
Jose Bueno <dev@jrbueno.com>
2024-12-02 21:42:46 -0500

    unfinished day11 from 2023

--- 2023/Day11/Solution.cs
+++ 2023/Day11/Solution.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Drawing;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Text;
@@ -10,7 +11,30 @@ namespace AdventOfCode.Y2023.Day11;
 [ProblemName("Cosmic Expansion")]
 class Solution : Solver {
 
-    public object PartOne(string input) {
+    public object PartOne(string input)
+    {
+        var data =
+            "...#......\n.......#..\n#.........\n..........\n......#...\n.#........\n.........#\n..........\n.......#..\n#...#.....";
+        var cosmos = data.Split("\n");
+        var emptyRows = cosmos
+            .Select((c, i) => new { Index = i, HasGalaxy = c.Contains('#') })
+            .Where(c => !c.HasGalaxy);
+        // var expanded = cosmos;
+        // for (int y = 0; y < cosmos.Length; y++)
+        // {
+        //     var emptyRow = cosmos[y].Contains('#');
+        //     for (int x = 0; x < cosmos[y].Length; x++)
+        //     {
+        //         var emptyCol =
+        //             cosmos.Select((c, i) => new { Index = i, EmptyGalaxy = c[x] == '#' })
+        //                 .Any(l => !l.EmptyGalaxy);
+        //     }
+        // }
+        var emptyCols = cosmos[0]
+            .Select((c, i) => new { Index = i, EmptyGalaxy = cosmos.All(l => l[i] != '#') })
+            .Where(x => x.EmptyGalaxy);
+        
+        Console.WriteLine(emptyCols.Count());
         return 0;
     }
 
